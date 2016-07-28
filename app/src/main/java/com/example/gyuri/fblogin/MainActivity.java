b71a2b92f26901b3156bf586cf097dabff52a5f7
bgyuri76 <bgyuridev@gmail.com>
2016-07-28 14:08:25 +0300

    working fb login (test app on fb)

---
+++ app/src/main/java/com/example/gyuri/fblogin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gyuri.fblogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this.getApplication());
+
+        setContentView(R.layout.activity_main);
+    }
+}
